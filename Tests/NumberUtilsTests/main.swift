8a151615867ad586457958b94bc57b20958d1e2a
Chuck Grindel <chuck@chuckgrindel.com>
2022-01-25 11:48:37 -0700

    Removed incorrect testcase declaration.

--- Tests/NumberUtilsTests/main.swift
+++ Tests/NumberUtilsTests/main.swift
@@ -2,7 +2,6 @@
   import XCTest
 
   XCTMain([
-    testCase(MessageTests.allTests),
     testCase(DecimalCSVFormatTests.allTests),
     testCase(DecimalRandomTests.allTests),
     testCase(DecimalRoundingTests.allTests),
